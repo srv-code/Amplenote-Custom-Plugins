49b86313e83cf8a35d95a28ce637e35b8d2efc51
Sourav Dey <sourav.blackjack@gmail.com>
2026-06-01 14:06:28 +0530

    Added 2 experimental settings
    - Added the setting options on NOTE.md
    - Added 2 experimental settings
    - Added the messages to constants and inserting the arguments using `String::replace` method
    - Added function to retrieve the setting values
    - Added the check to check if the context note is tagged with test
    - Added the original author's function of editing note's markdown content under experimental features
    - Added experimental edit selection markdown function

--- Edit As Markdown/src.js
+++ Edit As Markdown/src.js
@@ -1,11 +1,21 @@
 // JavaScript
 ({
   constants: {
+    settings: {
+      ENABLED_EXPERIMENTS: 'Enable experimental features? (y/n | default: n)',
+      ONLY_ALLOW_EXPERIMENTS_ON_TEST_NOTES: 'Only allow experimental features on notes tagged with `*/test/*`? (y/n | default: y)',
+    },
     messages: {
       UNHANDLED_ERROR: 'Something went wrong!\nPlease refer the console logs for the technical details and inform the developer.',
-      EDIT_NOTE_MARKDOWN: 'Edit note as Markdown',
-      ENTER_INSERTION_MARKDOWN: 'Enter markdown content to insert',
+      EDIT_NOTE_MARKDOWN: 'Edit note as Markdown:',
+      ENTER_INSERTION_MARKDOWN: 'Enter markdown content to insert:',
+      ENTER_REPLACEMENT_MARKDOWN: 'Edit markdown content to replace with:',
+      VIEWING_NOTE_MARKDOWN: 'Viewing note ($1 characters)',
+      VIEWING_SELECTION_MARKDOWN: 'Viewing selection ($1 characters)'
     },
+    _: {
+      TEST_TAG_NAME: 'test',
+    }
   },
 
   _handleError(app, error) {
@@ -14,10 +24,37 @@
     app.alert(this.constants.messages.UNHANDLED_ERROR);
   },
 
+  _getSettingValue(app, settingName, type = "string", defaultValue = null) {
+    // Ensure the provided key is one of the defined constant keys
+    if (!Object.values(this.constants.settings).includes(settingName))
+      throw new Error(`Invalid setting name '${settingName}' specified`);
+
+    // Validate the requested type
+    if (type !== "boolean" && type !== "string")
+      throw new Error(`Invalid type with value '${type}' specified`);
+
+    const val = app.settings[settingName];
+    if (type === "boolean") return val?.length > 0 ? val.trim().toLowerCase() === 'y' : (defaultValue || false);
+    if (type === "string") return val || defaultValue;
+  },
+
+  async _enableExperiments(app) {
+    const experimentsEnabled = this._getSettingValue(app, this.constants.settings.ENABLED_EXPERIMENTS, "boolean")
+    if(!experimentsEnabled) return false;
+    
+    const onlyOnTestNotes = this._getSettingValue(app, this.constants.settings.ONLY_ALLOW_EXPERIMENTS_ON_TEST_NOTES, "boolean", true)
+    const note = await app.notes?.find(app.context?.noteUUID);
+    const hasTestTag = note?.tags?.some(tag => tag.split('/').includes(this.constants._.TEST_TAG_NAME));
+
+    if(onlyOnTestNotes) return hasTestTag;
+    return true;
+  },
+
   noteOption: {
-    'Edit note': {
-      check(app) {
-        return false; // Disabled action added by original author
+    // EXPERIMENTAL FEATURE //
+    Edit: {
+      async check(app) {
+        return await this._enableExperiments(app);
       },
 
       async run(app, noteUUID) {
@@ -25,10 +62,10 @@
           const oldContent = await app.getNoteContent({ uuid: noteUUID })
           const newContent = await app.prompt(this.constants.messages.EDIT_NOTE_MARKDOWN, {
             inputs: [{ type: 'text', value: oldContent }]
-          })
-          if (newContent === null) return
+          });
+          if (newContent === null) return;
           
-          await app.replaceNoteContent({ uuid: noteUUID }, newContent)
+          await app.replaceNoteContent({ uuid: noteUUID }, newContent);
         } catch (error) {
           this._handleError(app, error);
         } finally {
@@ -41,7 +78,7 @@
       async run(app, noteUUID) {
         try {
           const content = await app.getNoteContent({ uuid: noteUUID });
-          await app.alert(content, { preface: `Viewing note (${content.length} characters)` });
+          await app.alert(content, { preface: this.constants.messages.VIEWING_NOTE_MARKDOWN.replace('$1', content.length) });
         } catch (error) {
           this._handleError(app, error);
         } finally {
@@ -56,8 +93,26 @@
       async run(app, text) {
         try {
           await app.alert(app.context.selectionContent, { 
-            preface: `Viewing selection (${app.context.selectionContent.length} characters)` 
+            preface: this.constants.messages.VIEWING_SELECTION_MARKDOWN.replace('$1', app.context.selectionContent.length)
           });
+        } catch (error) {
+          this._handleError(app, error);
+        }
+      },
+    },
+
+    Edit: {
+      // EXPERIMENTAL FEATURE //
+      async check(app) {
+        return await this._enableExperiments(app);
+      },
+
+      async run(app, text) {
+        try {
+          const newContent = await app.prompt(this.constants.messages.ENTER_REPLACEMENT_MARKDOWN, {
+            inputs: [{ type: 'text', value: app.context.selectionContent }]
+          });
+          if(typeof newContent === 'string') await app.context.replaceSelection(newContent);
         } catch (error) {
           this._handleError(app, error);
         }
