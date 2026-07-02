a1518a73dedae044faccfc4edcb74138fd811c8d
Sourav Dey <sourav.blackjack@gmail.com>
2026-07-02 13:05:39 +0530

    Code and comments beautification
    Broken long lines into manageable chunks

--- Generate TOC/src.js
+++ Generate TOC/src.js
@@ -2,21 +2,25 @@
 {
   constants: {
     settings: {
-      /** Title which should be displayed as the TOC section header. This is the anchoring header title, where future TOC updates will be made. Keep this unique to avoid ambiguity with other section titles. */
+      /** Title which should be displayed as the TOC section header. 
+       * This is the anchoring header title, where future TOC updates will be made. 
+       * Keep this unique to avoid ambiguity with other section titles. */
       TITLE: {
         label: "Title (string | default: TABLE OF CONTENTS)",
         defaultValue: 'TABLE OF CONTENTS',
         type: 'string',
       },
       
-      /** Resets the indentation of any level of heading if immediately following a break (e.g. horizontal line). */
+      /** Resets the indentation of any level of heading if immediately following 
+       * a break (e.g. horizontal line). */
       CONSIDER_HEADER_BREAKS: {
         label: "Consider Header Breaks (y/n | default: y)",
         defaultValue: 'y',
         type: 'boolean',
       },
 
-      /** Show a warning when duplicate titles are found and presented with two options, abort and proceed the operation. */
+      /** Show a warning when duplicate titles are found and presented with two options, 
+       * abort and proceed the operation. */
       WARN_ON_DUPLICATE_TITLES: {
         label: "Warn On Duplicate Titles (y/n | default: y)",
         defaultValue: 'y',
@@ -50,12 +54,14 @@
       ERROR_INVALID_BOOLEAN_SETTING: 'Invalid value provided for "$1". Please enter \'y\' or \'n\'.',
       ERROR_DUPLICATE_SECTION_TITLE: 'Error: Duplicate Section Titles',
       ERROR_DUPLICATE_SECTION_BODY: 'Below sections have duplicate titles.\n' 
-            + '(Hint: Fix them either by changing to a different title or pad them using spaces. Creating TOC now will result in ambiguous linkage.)\n\n' 
+            + '(Hint: Fix them either by changing to a different title or pad them using spaces. '
+            + 'Creating TOC now will result in ambiguous linkage.)\n\n' 
             + '-- Section Title,  Header Level,  Instance Count --\n' 
             + '$1\n',
       ERROR_OFFENDING_SECTION_TITLE: 'Error: Problematic Section Titles',
       ERROR_OFFENDING_SECTION_BODY: 'Below sections have problematic titles.\n' 
-            + '(Hint: Fix them by eliminating the offending characters from their titles. Creating TOC now will result in broken linkage.)\n\n' 
+            + '(Hint: Fix them by eliminating the offending characters from their titles. ' 
+            + 'Creating TOC now will result in broken linkage.)\n\n' 
             + '-- Section Title,  Header Level, Offending Character(s) and Count --\n'
             + '$1\n',
       ERROR_INTERNAL_TITLE: 'Internal Error Occurred',
@@ -72,6 +78,10 @@
 
       /** Text color for the last update message */
       LAST_UPDATE_COLOR: '#777879',
+      LAST_UPDATE_COLOR_CYCLE: "44",
+
+      /** Tab character for indentation */
+      TAB: '    ',
     },
   },
 
@@ -240,11 +250,34 @@
         minute: "2-digit",
         hour12: true,
       }).format(new Date());
-    return `*<mark style="color:${this.constants._.LAST_UPDATE_COLOR};">Last updated: ${datetime}<!-- {"cycleColor":"44"} --></mark>*\n`
+
+    return `*<mark style="color:${
+        this.constants._.LAST_UPDATE_COLOR
+      };">Last updated: ${
+        datetime
+      }<!-- {"cycleColor": "${
+        this.constants._.LAST_UPDATE_COLOR_CYCLE
+      }"} --></mark>*\n`;
   },
 
-  _generateTOCFromSections(noteUUID, title, sections, considerHeaderBreaks, unordered, hideLastUpdateMessage, insertOnly = false) {
-    console.groupCollapsed('_generateTOCFromSections', { noteUUID, title, sections, considerHeaderBreaks, unordered, hideLastUpdateMessage, insertOnly });
+  _generateTOCFromSections(
+    noteUUID, 
+    title, 
+    sections, 
+    considerHeaderBreaks, 
+    unordered, 
+    hideLastUpdateMessage, 
+    insertOnly = false,
+  ) {
+    console.groupCollapsed('_generateTOCFromSections', { 
+      noteUUID, 
+      title, 
+      sections, 
+      considerHeaderBreaks, 
+      unordered, 
+      hideLastUpdateMessage, 
+      insertOnly 
+    });
     
     let content = hideLastUpdateMessage ? '' : this._getLastUpdateMessage(new Date());
     const indexes = { 1: null, 2: null, 3: null };
@@ -255,14 +288,25 @@
     for(const { heading, index } of sections) {
       const isTOC = insertOnly ? false : this._isTOCHeader(heading, title);
       if(!!heading && !!heading.text.trim() && (!isTOC || (isTOC && !!section))) {
-        const headingLevel = considerHeaderBreaks ? (sectionIndex > 0 && sections[sectionIndex-1].heading === null ? 1 : heading.level) : heading.level;
-        let line = '>.  ' + '    '.repeat(headingLevel-1) + `${this._getNextIndex(indexes, headingLevel, unordered)}  [${heading.text.trim()}]`;
-        if(this.constants._.INVALID_SECTION_TITLE_CHARS.find(ch => heading.anchor.includes(ch))) line += `(#${heading.anchor})`;
-        else line += `(https://www.amplenote.com/notes/${noteUUID}#${heading.anchor})`;
+        const headingLevel = 
+          considerHeaderBreaks 
+            ? (sectionIndex > 0 && sections[sectionIndex-1].heading === null ? 1 : heading.level) 
+            : heading.level;
+        let line = 
+          '>.  ' 
+          + this.constants._.TAB.repeat(headingLevel-1) 
+          + `${this._getNextIndex(indexes, headingLevel, unordered)}  [${heading.text.trim()}]`;
+
+        if(this.constants._.INVALID_SECTION_TITLE_CHARS.find(ch => heading.anchor.includes(ch))) 
+          line += `(#${heading.anchor})`;
+        else 
+          line += `(https://www.amplenote.com/notes/${noteUUID}#${heading.anchor})`;
         line += '\n>\n';
-        console.info('  >> line:: %O (H%d)', line, heading.level);
+        
         content += line;
+        console.info('  >> line:: %O (H%d)', line, heading.level);
       }
+      
       if(!section && isTOC) {
         section = { heading, index };
         console.info('  >> found section::', section);
@@ -344,12 +388,19 @@
       console.log('Sections', sections);
 
       if(this._getSettingValue(app, 'WARN_ON_DUPLICATE_TITLES')) {
-        const dupSections = sections.filter(sec => sec.heading != null && sec.heading.text.length > 0 && sec.index != null);
+        const dupSections = sections.filter(sec => 
+          sec.heading != null 
+          && sec.heading.text.length > 0 
+          && sec.index != null
+        );
+
         if(dupSections.length > 0) {
           console.log('Duplicate Sections', dupSections);
           const message = this.constants.messages.ERROR_DUPLICATE_SECTION_BODY.replace(
               '$1', 
-              dupSections.reduce((text, sec, index) => `${text}\n${index+1}.  '${sec.heading.text}',  H${sec.heading.level},  ${sec.index}`, ''),
+              dupSections.reduce((text, sec, index) => 
+                `${text}\n${index+1}.  '${sec.heading.text}',  H${sec.heading.level},  ${sec.index}`, 
+              ''),
             );
           const response = await app.alert(
             message,
@@ -448,8 +499,10 @@
   noteOption: {
     /**
      * This is the main trigger option of the plugin. 
-     * It first checks for the presence of the TOC. If not present, it simply attaches at the top of the note content. 
-     * If the TOC section is present (checks only for the first occurrence), it updates in the existing section inline. 
+     * It first checks for the presence of the TOC. 
+     * If not present, it simply attaches at the top of the note content. 
+     * If the TOC section is present (checks only for the first occurrence), 
+     * it updates in the existing section inline. 
      * */
     async run(app, noteUUID) {
       console.groupCollapsed('TOC plugin | running from noteOption', { noteUUID });
