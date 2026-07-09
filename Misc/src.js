74a0c76458caa8681b7de24c6b9f86cae1d287b5
Sourav Dey <sourav.blackjack@gmail.com>
2026-07-09 13:15:54 +0530

    Renamed Miscellaneous to Misc. Added new text substitution commands.

---
+++ Misc/src.js
@@ -0,0 +1,59 @@
+// JavaScript
+{
+  replaceText: {
+    'Uppercase': {
+      async run(app, text) {
+        await app.context.replaceSelection(text.toUpperCase());
+      },
+    },
+
+    'Lowercase': {
+      async run(app, text) {
+        await app.context.replaceSelection(text.toLowerCase());
+      },
+    },
+
+    'Capitalize': {
+      async run(app, text) {
+        await app.context.replaceSelection(text.replace(/\b\w/g, (c) => c.toUpperCase()));
+      },
+    },
+
+    'Delete': {
+      async run(app, text) {
+        await app.context.replaceSelection('');
+      },
+    },
+  },
+
+  insertText: {
+    /* Directional arrows */
+    'arrow-right': {
+      run(app) { return '→'; },
+    },
+
+    'left-arrow': {
+      run(app) { return '←'; },
+    },
+
+    'hyphen': {
+      run(app) { return '－'; },
+    },
+
+    'arrow-up': {
+      run(app) { return '↑'; },
+    },
+
+    'arrow-down': {
+      run(app) { return '↓'; },
+    },
+
+    'arrow-horizontal': {
+      run(app) { return '↔'; },
+    },
+
+    'arrow-vertical': {
+      run(app) { return '↕'; },
+    },
+  },
+}
