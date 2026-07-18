c36eb0df68aabc6f225a4908057b1a5ba83ed63d
Sourav Dey <sourav.blackjack@gmail.com>
2026-07-18 09:57:52 +0530

    Added commands <-, ->, --; Also fixed the 'Uppercase', 'Lowercase' and 'Capitalize' commands

--- Misc/src.js
+++ Misc/src.js
@@ -2,30 +2,56 @@
 {
   replaceText: {
     'Uppercase': {
-      async run(app, text) {
-        await app.context.replaceSelection(text.toUpperCase());
+      async run(app) {
+        await app.context.replaceSelection(app.context.selectionContent.toUpperCase());
       },
     },
 
     'Lowercase': {
-      async run(app, text) {
-        await app.context.replaceSelection(text.toLowerCase());
+      async run(app) {
+        await app.context.replaceSelection(app.context.selectionContent.toLowerCase());
       },
     },
 
     'Capitalize': {
-      async run(app, text) {
+      async run(app) {
         await app.context.replaceSelection(
-          text.replace(/[a-zA-Z]+/g, (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+          app.context.selectionContent
+            .replace(/[a-zA-Z]+/g, (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
         );
       },
     },
 
     'Delete': {
-      async run(app, text) {
+      async run(app) {
         await app.context.replaceSelection('');
       },
     },
+
+    // 'Copy Section Link': {
+    //   async check(app, text) {
+    //     console.log('>> [check] text: %s, selectionContent: %s', text, app.context.selectionContent);
+    //     const valid = app.context.selectionContent.trimStart().startsWith('#');
+    //     console.log('>> [check] valid: %s', valid);
+    //     return valid;
+    //   },
+    //   async run(app, text) {
+    //     console.log('>> [run] text: %s, selectionContent: %s | url: %s, link: %s, noteUUID: %s', 
+    //       text, app.context.selectionContent, app.context.url, app.context.link, app.context.noteUUID);
+        
+    //     const sectionLink = `${app.context.link}#`;
+    //     // await this._copyToClipboard(sectionLink);
+
+    //     // await app.writeClipboardData(
+    //     //   '',
+    //     //   "text/plain",
+    //     // );
+
+
+    //     // await app.alert(`Copied section link: ${sectionLink}`);
+        
+    //   },
+    // },
   },
 
   insertText: {
@@ -34,11 +60,23 @@
       run(app) { return '→'; },
     },
 
+    '->': {
+      run(app) { return '→'; },
+    },
+
     'left-arrow': {
       run(app) { return '←'; },
     },
 
+    '<-': {
+      run(app) { return '←'; },
+    },
+
     'hyphen': {
+      run(app) { return '－'; },
+    },
+
+    '--': {
       run(app) { return '－'; },
     },
 
